0a722abd51fa7988be3ea4e8cedde3011e95e888
unknown <nina.heuzer@student.vinci.be>
2021-12-08 13:34:26 +0100

    connection : tailwind c'est pas encore ça

--- components/ConnectionRegistration/Connection.jsx
+++ components/ConnectionRegistration/Connection.jsx
@@ -7,91 +7,71 @@ const Connection = () => {
             Se connecter
           </h2>
         </div>
-        <form
-          className="shadow px-4 py-5 mt-8 space-y-6"
-          action="#"
-          method="POST"
-        >
-          <input type="hidden" name="remember" value="true" />
-          <div className="rounded-md -space-y-px">
-            <div>
-              <label htmlFor="email-address-connection" className="sr-only">
-                Adresse email ou pseudo
-              </label>
-              <input
-                id="email-address-connection"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-green-700 focus:border-green-700 focus:z-10 sm:text-sm"
-                placeholder="Email ou pseudo"
-              />
-            </div>
-            <div>
-              <label htmlFor="password-connection" className="sr-only">
-                Mot de passe
-              </label>
-              <input
-                id="password-connection"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-green-700 focus:border-green-700 focus:z-10 sm:text-sm"
-                placeholder="Password"
-              />
-            </div>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                className="h-4 w-4 text-green-800 focus:ring-green-700 border-gray-300 rounded"
-              />
-              <label
-                htmlFor="remember-me"
-                className="ml-2 block text-sm text-gray-900"
-              >
-                Se souvenir de moi
-              </label>
-            </div>
-
-            <div className="text-sm">
-              <a
-                href="#"
-                className="font-medium text-green-800 hover:text-green-700"
-              >
-                Vous avez oublié votre mot de passe ?
-              </a>
-            </div>
-          </div>
-
-          <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-800 hover:bg-green-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-700"
-            >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <svg
-                  className="h-5 w-5 text-green-700 group-hover:text-green-700"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clipRule="evenodd"
+        <form method="POST">
+          <div className="shadow overflow-hidden sm:rounded-md">
+            <div className="px-4 py-5 bg-white sm:p-6">
+              <div className="shadow-sm -space-y-px">
+                <div>
+                  <label htmlFor="email-address-connection" className="sr-only">
+                    Adresse email ou pseudo
+                  </label>
+                  <input
+                    id="email-address-connection"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="appearance-none rounded-t-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-700 focus:border-green-700 focus:z-10 sm:text-sm"
+                    placeholder="Email ou pseudo"
                   />
-                </svg>
-              </span>
-              Sign in
-            </button>
+                </div>
+                <div>
+                  <label htmlFor="password-connection" className="sr-only">
+                    Mot de passe
+                  </label>
+                  <input
+                    id="password-connection"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="appearance-none rounded--b-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-700 focus:border-green-700 focus:z-10 sm:text-sm"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+              <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 text-green-800 focus:ring-green-700 border-gray-300 rounded"
+                  />
+                  <label
+                    htmlFor="remember-me"
+                    className="ml-2 block text-sm text-gray-900"
+                  >
+                    Se souvenir de moi
+                  </label>
+                </div>
+
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-medium text-green-800 hover:text-green-700"
+                  >
+                    Vous avez oublié votre mot de passe ?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white focus:border-green-700 bg-green-800 hover:bg-green-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-700"
+                >
+                  Sign in
+                </button>
+              </div>
+            </div>
           </div>
         </form>
       </div>
