fb6cb608d4c7e48d23fb49dd0488c744d63b0967
bdeTheux <boris.detheux@student.vinci.be>
2021-12-08 13:52:57 +0100

    Fonctionality - create work with the fake api

--- pages/api/posts/posts.js
+++ pages/api/posts/posts.js
@@ -3,5 +3,22 @@ import { posts } from "../../../data";
 //api fictive
 
 export default function handler(req, res) {
-  res.status(200).json(posts);
+  if (req.method === "GET"){
+    res.status(200).json(posts);
+  }else if (req.method === "POST"){
+    const post = req.body.newPostSubmit
+    console.log("post api : " + post)
+    const newPost = {
+      "id" : 3,
+      "title" : post.title,
+            "description" : post.description,
+            "postNature" : post.postNature,
+            "price" : post.price,
+            "image" : post.image,
+            "campus" : post.campus,
+    }
+    console.log(newPost)
+    posts.push(newPost);
+    res.status(201).json(newPost)
+  }
 }
--- pages/newPost.js
+++ pages/newPost.js
@@ -1,45 +1,87 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 
 const newPost = () => {
 
     const [posts, setPosts] = useState([]);
-    const [newPosts, setNewPosts] = useState('');
+    const [newPost, setNewPost] = useState('');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState("")
+    const [postNature, setPostNature] = useState("")
+    const [price, setPrice] = useState("")
+    const [image, setImage] = useState("")
+    const [campus, setCampus] = useState("")
+
+    const campusList = ["Woluwe", "LLN", "Ixelles"]
+
     const getStaticProps = async () => {
-        const res = await fetch("http//localhost:3000")
+        const res = await fetch(process.env.URL_FRONT + `/api/posts/posts`)
+        const data = await res.json()
+        setPosts(data);
+        
+    }
+    const submitPost = async () => {
+        console.log(title)
+        let newPostSubmit = {
+            "title" : title,
+            "description" : description,
+            "postNature" : postNature,
+            "price" : price,
+            "image" : image,
+            "campus" : campus,
+        }
+        console.log("before fetch " + JSON.stringify({newPostSubmit}))
+        const res = await fetch(`http://localhost:3000/api/posts/posts`, {
+            method: 'POST',
+            body: JSON.stringify({newPostSubmit}),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+        console.log(newPostSubmit)
+        const data = await res.json()
+        console.log(data)
     }
 
-
-
+    const campusHandler = (val) => {
+        
+        setCampus(val.target.value)
+    }
 
     return(
 
         <section className="flex-auto">
             <h1>Crée une annonce</h1>
             <div className="flex-grow bg-green-800 rounded mx-auto bg-center w-7/12 h-full mt-48">
-                <form className="flex">
+                
+                <form action="post" className="flex">
                     <label>
-                        Titre:<input name="title" type="text" placeholder='titre' required className="flex appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"/>
+                        Titre:<input  onChange={val => setTitle(val.target.value)} name="title" type="text" placeholder='titre' required className="flex appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"/>
                     </label>
 
                     <label>
-                        Description:<textarea name="description" rows="3" placeholder='description' required className="flex appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"/>
+                        Description:<textarea value={description} onChange={val => setDescription(val.target.value)} name="description" rows="3" placeholder='description' required className="flex appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"/>
                     </label>
+                    <div>
+                        <label> A vendre: </label><input value={postNature} onChange={val => setPostNature(val.target.value)} name="postNature" type="radio" value="sell" required/>
+                        <label> A donner: </label><input value={postNature} onChange={val => setPostNature(val.target.value)} name="postNature" type="radio" value="given" required/>
+                    </div>
                     <label>
-                        Prix:<input name="price" type="number" placeholder='0.0€' required className="flex appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"/>
+                        Prix:<input value={price} onChange={val => setPrice(val.target.value)} name="price" type="number" placeholder='0.0€' required className="flex appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"/>
                     </label>
-                    
+
                     <label>Campus:
-                        <select name="campus" required className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                            <option value="Woluwe" className="bg-gray-200 text-gray-700">Woluwe</option>
-                            <option value="LLN" className="bg-gray-200 text-gray-700">Louvain-La-Neuve</option>
-                            <option value="Ixelles" className="bg-gray-200 text-gray-700">Ixelles</option>
+                        <select name="campus" onChange={campusHandler} required className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            {campusList.map((campu, index) => {
+                            return <option key={index} className="bg-gray-200 text-gray-700">{campu}</option>;
+                            })}
                         </select>
                     </label>
+                     
                     <label className="flex md:w-44 bg-gray-200 rounded text-gray-700">
-                    Dossier a telecharger:<input name="file" type="file" multiple className="hidden" required/>
+                    Dossier a telecharger:<input value={image} onChange={val => setImage(val.target.value)} name="file" type="file" multiple className="hidden" required/>
                     </label>
-                    
-                    <button name="submitPost" type="submit" className="shadow bg-green-700 rounded hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4">Crée</button>
+                                        
+                    <button name="submitPost" onClick={submitPost} type="button" className="shadow bg-green-700 rounded hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4">Crée</button>
                 </form>
             </div>
             <p className="flex-auto mx-auto">Votre annonce sera soumise à un modérateur.<br/>
@@ -49,4 +91,4 @@ const newPost = () => {
     )
 }
 
-export default newPost;
+export default newPost; 
