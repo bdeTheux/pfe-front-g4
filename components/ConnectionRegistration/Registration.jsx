8ab8aa59f9476970c5632a56930cd60fc83a5e6c
unknown <nina.heuzer@student.vinci.be>
2021-12-08 09:40:54 +0100

    connectionRegistration : add form but not fonctionnal

--- components/ConnectionRegistration/Registration.jsx
+++ components/ConnectionRegistration/Registration.jsx
@@ -1,2 +1,113 @@
-const Registration = () => {};
+const Registration = () => {
+  return (
+    <div class="mt-10 sm:mt-0">
+      <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div class="md:col-span-1">
+          <div class="px-4 sm:px-0">
+            <h3 class="text-lg font-medium leading-6 text-gray-900">
+              S'inscrire
+            </h3>
+          </div>
+        </div>
+        <div class="mt-5 md:mt-0 md:col-span-2">
+          <form action="#" method="POST">
+            <div class="shadow overflow-hidden sm:rounded-md">
+              <div class="px-4 py-5 bg-white sm:p-6">
+                <div class="grid grid-cols-6 gap-6">
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="first-name"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      Prénom
+                    </label>
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autocomplete="given-name"
+                      class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="last-name"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      Nom
+                    </label>
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autocomplete="family-name"
+                      class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-4">
+                    <label
+                      for="email-address"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      Email vinci
+                    </label>
+                    <input
+                      type="text"
+                      name="email-address"
+                      id="email-address"
+                      autocomplete="email"
+                      class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="country"
+                      class="block text-sm font-medium text-gray-700"
+                    >
+                      Campus
+                    </label>
+                    <select
+                      id="country"
+                      name="country"
+                      autocomplete="country-name"
+                      class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    >
+                      <option>Woluwé</option>
+                      <option>Louvain-La-Neuve</option>
+                      <option>Ixelles</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="mb-6">
+                  <label
+                    class="block text-gray-700 text-sm font-bold mb-2"
+                    for="password"
+                  >
+                    Mot de passe
+                  </label>
+                  <input
+                    class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    id="password"
+                    type="password"
+                  />
+                </div>
+              </div>
+              <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <button
+                  type="submit"
+                  class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  S'inscrire
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Registration;
