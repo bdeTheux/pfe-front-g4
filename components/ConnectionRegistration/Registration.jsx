86f09658da2d375359b93ce423b2e0e45029a1ae
unknown <nina.heuzer@student.vinci.be>
2021-12-09 08:55:35 +0100

    register - that works i think

--- components/ConnectionRegistration/Registration.jsx
+++ components/ConnectionRegistration/Registration.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react";
+
 const Registration = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [campus, setCampus] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onRegister = async () => {
+    const newUser = {
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      campus: campus,
+      password: password,
+    };
+    console.log("new user : ", newUser);
+    const res = await fetch("http://localhost:3000/api/users/register", {
+      //https://pfe-back-g4-dev.herokuapp.com/signup
+      method: "POST",
+      body: JSON.stringify({ newUser }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -7,7 +34,7 @@ const Registration = () => {
             S'inscrire
           </h2>
         </div>
-        <form action="#" method="POST">
+        <form method="POST">
           <div className="shadow overflow-hidden sm:rounded-md">
             <div className="px-4 py-5 bg-white sm:p-6">
               <div className="shadow-sm -space-y-px">
@@ -23,6 +50,7 @@ const Registration = () => {
                     required
                     className="appearance-none rounded-t-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-700 focus:border-green-700 focus:z-10 sm:text-sm"
                     placeholder="Prénom"
+                    onChange={(e) => setFirstName(e.target.value)}
                   />
                 </div>
                 <div>
@@ -37,6 +65,7 @@ const Registration = () => {
                     required
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-700 focus:border-green-700 focus:z-10 sm:text-sm"
                     placeholder="Nom"
+                    onChange={(e) => setLastName(e.target.value)}
                   />
                 </div>
                 <div>
@@ -55,6 +84,7 @@ const Registration = () => {
                     required
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-700 focus:border-green-700 focus:z-10 sm:text-sm"
                     placeholder="Email vinci"
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                 </div>
                 <div>
@@ -68,6 +98,7 @@ const Registration = () => {
                     autoComplete="campus"
                     className="mt-1 block w-full py-2 px-3 border rounded-none border-gray-300 bg-white shadow-sm focus:outline-none focus:ring-green-700 focus:border-green-700 sm:text-sm"
                     defaultValue={"DEFAULT"}
+                    onChange={(e) => setCampus(e.target.value)}
                   >
                     <option value="DEFAULT" disabled selected>
                       Sélectionnez votre campus
@@ -90,6 +121,7 @@ const Registration = () => {
                   required
                   className="appearance-none rounded-b-md  relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-green-700 focus:border-green-700 focus:z-10 sm:text-sm"
                   placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
             </div>
@@ -97,6 +129,7 @@ const Registration = () => {
               <button
                 type="submit"
                 className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white focus:border-green-700 bg-green-800 hover:bg-green-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-700"
+                onClick={onRegister}
               >
                 S'inscrire
               </button>
