1c9d7e5e6e3dc7bd91baab77f37ea8c80cebaf16
samyAlliche <samy.alliche.22@gmail.com>
2021-12-10 10:17:47 +0100

    post - added campus badge

--- components/Post/Post.jsx
+++ components/Post/Post.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { LocationMarkerIcon } from "@heroicons/react/solid";
 
 /*
 <p className="px-2 py-1 relative top-7 inset-x-0 w-max z-20  opacity-80 rounded-full bg-black text-white text-sm font-mono">
@@ -16,6 +17,10 @@ const Post = ({ post }) => {
       }}
     >
       <a key={post.id} className="group">
+      <p className="px-2 py-1 relative top-10 left-3 inset-x-0 w-max z-20  bg-black text-white text-sm font-mono">
+        <LocationMarkerIcon className="w-4 h-4 inline mr-1 mb-0.5"></LocationMarkerIcon>
+            {post.campus}
+          </p>
         <div className="w-full aspect-w-5 aspect-h-5 bg-gray-200 rounded-3xl overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
           <Image
             src={post.image}
@@ -24,7 +29,7 @@ const Post = ({ post }) => {
             className="w-full h-full object-center object-cover group-hover:opacity-75 transition duration-300 ease-in-out"
           />
         </div>
-        <h3 className="mt-4 text-gray-700 font-extrabold">{post.title}</h3>
+        <h3 className="text-gray-700 font-extrabold mt-2">{post.title}</h3>
         <p className="mt-1 text-lg text-sm md:text-medium font-light text-gray-900">
           {post.price}€
         </p>
