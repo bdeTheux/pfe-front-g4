1f5e6288d450925037a58ddaa51276ea0904ac82
unknown <nina.heuzer@student.vinci.be>
2021-12-11 16:49:03 +0100

    delete buttonmail example

--- pages/buttonmail/buttonmail.js
+++
@@ -1,8 +0,0 @@
-import ButtonMailTo from "../../components/ButtonMailTo/ButtonMailTo";
-export default function buttonmail() {
-  return (
-    <p>
-      llsdqksxfaemrfjzejkl fzlej f<ButtonMailTo mailto="nina.heuzer@yahoo.fr" />
-    </p>
-  );
-}
