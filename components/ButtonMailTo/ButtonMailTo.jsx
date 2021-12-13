6a2149d276e0258ec69885530e64166b88fda037
unknown <nina.heuzer@student.vinci.be>
2021-12-13 14:14:58 +0100

    button mail ok

--- components/ButtonMailTo/ButtonMailTo.jsx
+++ components/ButtonMailTo/ButtonMailTo.jsx
@@ -1,15 +1,15 @@
 import { MailIcon } from "@heroicons/react/outline";
+import Link from "next/link";
 const ButtonMailTo = ({ mailto, title }) => {
   //add post to param
   const ref = `mailto:${mailto}?subject=Achat ${title}`;
   return (
     <div>
-      <button
-        href={ref}
-        className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"
-      >
-        Envoyer un mail <MailIcon className="w-6 h-6 text-white ml-2" />
-      </button>
+      <Link href={ref} passHref={true}>
+        <a className="flex justify-between ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
+          Envoyer un mail <MailIcon className="w-6 h-6 text-white ml-2" />
+        </a>
+      </Link>
     </div>
   );
 };
