f1501a3cad8e48918f4d69202ea0fd5482d56c9c
unknown <nina.heuzer@student.vinci.be>
2021-12-15 22:38:55 +0100

    mooie about pagina

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,69 +1,39 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-
-const Box = styled.div`
-  position: fixed;
-  z-index: 0;
-  top: 0;
-`;
 
 const About = () => {
-  function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height,
-    };
-  }
-
-  const [width, setWidth] = useState();
-  const [height, setheight] = useState();
-
-  useEffect(() => {
-    const { width, height } = getWindowDimensions();
-    setWidth(width);
-    setheight(height);
-  }, []);
-
-  useEffect(() => {
-    function handleResize() {
-      const { width, height } = getWindowDimensions();
-
-      setWidth(width);
-
-      setheight(height);
-    }
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  if (width && height) {
-    return (
-      <div>
-        <Box>
-          <Image src="/images/about2.png" width={width} height={height} />
-        </Box>
-        <div className="flex align-middle ">
-          <p>
-            À propos de nous : VinciMarket est une plateforme à destination de
-            tous les membres de la Communauté Vinci qui souhaitent vendre,
-            donner ou acheter des objets en attente d’une seconde vie. Ce projet
-            s’inscrit dans une démarche de développement durable visant à
-            promouvoir le réemploi et le recyclage. VinciMarket favorise donc la
-            récupération, le recyclage, la revalorisation d’objets et de
-            produits mais également les liens sociaux via des échanges et
-            contacts entre les membres d’une même communauté. Vous pouvez, vous
-            aussi, intégrer la plateforme et mettre du vôtre en accordant une
-            deuxième chance à vos objets ou ceux des autres.
-          </p>
-        </div>
+  return (
+    <div className="">
+      <div className="z-0">
+        <Image
+          src="/images/about2.png"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+          className="z-0 h-screen"
+        />
       </div>
-    );
-  }
-  return null;
+      <div className="flex flex-col justify-center relative mx-10 h-screen items-center z-50 text-xl text-white">
+        <h1 className="inline text-2xl font-semibold leading-none text-center">
+          À propos de nous
+        </h1>
+        <br></br>
+        <p className="mx-60 text-center tracking-wide">
+          VinciMarket est une plateforme à destination de tous les membres de la
+          Communauté Vinci qui souhaitent vendre, donner ou acheter des objets
+          en attente d’une seconde vie. Ce projet s’inscrit dans une démarche de
+          développement durable visant à promouvoir le réemploi et le recyclage.
+          VinciMarket favorise donc la récupération, le recyclage, la
+          revalorisation d’objets et de produits mais également les liens
+          sociaux via des échanges et contacts entre les membres d’une même
+          communauté. Vous pouvez, vous aussi, intégrer la plateforme et mettre
+          du vôtre en accordant une deuxième chance à vos objets ou ceux des
+          autres.
+        </p>
+      </div>
+    </div>
+  );
 };
+
 export default About;
 
 /*
