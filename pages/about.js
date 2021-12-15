fa9ff1ccfc034b48c37f90226c75c1408adf5580
unknown <nina.heuzer@student.vinci.be>
2021-12-15 19:48:03 +0100

    about us

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 export default function about() {
-  return <div class="grid grid-cols-2">
-    <div id="photo">
-      <Image
+  return (
+    <div class="flex flex-col">
+      <div id="photo">
+        <Image src="/images/about.jpg" layout="fill"></Image>
+      </div>
+      <div id="aboutText">
+        <Image src="/images/about.jpg" layout="fill"></Image>
+      </div>
     </div>
-  </div>;
+  );
 }
 /*
 <div className="flex flex-col min-h-screen m-20 mt-20 pl-10 py-2 md:py-6 bg-gray-200 rounded-lg">
