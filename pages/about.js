fa3b9c0a879fe9aa5ecfbcf563ea9eadc7b0b545
unknown <nina.heuzer@student.vinci.be>
2021-12-15 18:15:19 +0100

    debut page about

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,14 @@
+import Image from "next/image";
 export default function about() {
-  return (
-    <div className="flex flex-col min-h-screen m-20 mt-20 pl-10 py-2 md:py-6 bg-gray-200 rounded-lg">
+  return <div class="grid grid-cols-2">
+    <div id="photo">
+      <Image
+    </div>
+  </div>;
+}
+/*
+<div className="flex flex-col min-h-screen m-20 mt-20 pl-10 py-2 md:py-6 bg-gray-200 rounded-lg">
       <h2 className="text-4xl font-extrabold">Titre</h2>
       <div>test</div>
     </div>
-  );
-}
+*/
